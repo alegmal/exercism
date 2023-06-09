0af04d60500b937cc50821ee120c211d1249457d
Aleg Malinovsky <6178232+alegmal@users.noreply.github.com>
2023-06-10 00:40:45 +0300

    removed main

--- go/vehicle-purchase/vehicle_purchase.go
+++ go/vehicle-purchase/vehicle_purchase.go
@@ -2,15 +2,35 @@ package purchase
 
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	panic("NeedsLicense not implemented")
+	if kind == "car" || kind == "truck" {
+		return true
+	}
+
+	return false
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+	const text = " is clearly the better choice."
+
+	if option1 < option2 {
+		return option1 + text
+	}
+
+	return option2 + text
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
-	panic("CalculateResellPrice not implemented")
+	var reductionPercent float64
+
+	if age < 3 {
+		reductionPercent = 0.8
+	} else if age >= 3 && age < 10 {
+		reductionPercent = 0.7
+	} else if age >= 10 {
+		reductionPercent = 0.5
+	}
+
+	return originalPrice * reductionPercent
 }
