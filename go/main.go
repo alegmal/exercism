53da3e21f2c9f8e9693ef3938cfdf28a7739d23c
Aleg Malinovsky <6178232+alegmal@users.noreply.github.com>
2023-06-09 11:57:28 +0300

    another exercise

---
+++ go/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	message := `
+**************************
+*    BUY NOW, SAVE 10%   *
+**************************
+`
+	fmt.Println(message)
+	
+	fmt.Println(trimmed)
+}
